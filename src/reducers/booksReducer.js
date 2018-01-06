bfa6c62538e5f290990fa02f62c6d6bf17f53f75
Gitster-Mohith <saipalivela93@gmail.com>
2018-01-06 03:32:35 -0500

    Initial Commit with Redux

---
+++ src/reducers/booksReducer.js
@@ -0,0 +1,9 @@
+export default function () {
+	return [
+		{title: 'The Immortals of Meluha'},
+		{title: 'The Secret of Nagas'},
+		{title: 'The Oath of the Vayuputras'},
+		{title: 'The Scion of Ikshvaku'},
+		{title: 'The Warrior of Mithila'}
+	];
+}
